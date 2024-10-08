9beeab73b010fbf563fc69dea7201682f5674826
tribuanaayu <straidr222@gmail.com>
2024-10-08 11:05:16 +0700

    SS
    S

--- myapp/app.js
+++ myapp/app.js
@@ -3,24 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const expressLayout = require("express-ejs-layouts");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/aboutus');
+var prodiRouter = require('./routes/prodi');
+var contactRouter = require('./routes/contact');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayout);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/aboutus', usersRouter);
+app.use('/prodi', usersRouter);
+app.use('/contact', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
