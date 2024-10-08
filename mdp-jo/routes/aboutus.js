6870fa8acf55652d04591d90caf13b97f55b9ea0
Jovan <kimpan.yusa@gmail.com>
2024-10-08 14:26:10 +0700

    penghapusan files

--- mdp-jo/routes/aboutus.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/about', function(req, res, next) {
-   res.render('aboutus', {title: 'halaman about us', layout: 'main' });
-});
-
-module.exports = router;
